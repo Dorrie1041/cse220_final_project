bc363529f21be23f7a03de74415489b5a1edd7d1
Dorrie Shen <dongrui.shen@gmail.com>
2024-11-18 15:58:33 -0800

    implement perceptron

---
+++ src/bp/perceptron.cc
@@ -0,0 +1,10 @@
+#include "perceptron.h"
+
+#include <iostream>
+
+extern "C" {
+#include "bp.param.h"
+#include "core.param.h"
+#include "globals/assert.h"
+#include "table_info.h"
+}
---
+++ src/bp/perceptron.h
@@ -0,0 +1,26 @@
+
+#ifndef __PREDICTOR_H__
+#define __PREDICTOR_H__
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include "bp/bp.h"
+
+/*************Interface to Scarab***************/
+void bp_perceptron_init();
+void bp_perceptron_timestamp(Op* op);
+uns8 bp_perceptron_pred(Op*);
+void bp_perceptron_spec_update(Op* op);
+void bp_perceptron_update(Op* op);
+void bp_perceptron_retire(Op* op);
+void bp_perceptron_recover(Recovery_Info*);
+uns8 bp_perceptron_full(uns proc_id);
+
+#ifdef __cplusplus
+}
+
+#endif
+
+#endif
